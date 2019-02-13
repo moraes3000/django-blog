dca8094ac3497e45f57a2ed5e8376f74c4370c82
Bruno Moraes <moraes3000@gmail.com>
2019-02-12 22:21:53 -0200

    ajustes crud bannerfoto

--- banner/urls.py
+++ banner/urls.py
@@ -5,13 +5,12 @@ urlpatterns = [
     path('novo', views.BannerCreateView.as_view(), name='BannerCreateView'),
     path('lista', views.BannerListView.as_view(), name='BannerListView'),
     path('delete/<int:pk>', views.BannerDeleteView.as_view(), name='BannerDeleteView'),
-    path('update/<int:pk>', views.BannerUpdate.as_view(), name='BannerUpdate')
-    
-    # #admin
-    # path('admin-post', views.AdminPostView.as_view(), name='admin-post'),
-    # path('novo/', views.PostCreateView.as_view(), name='PostCreateView'),
-    # path('delete/<int:pk>', views.PostDeleteView.as_view(), name='delete-post'),
-    # path('admin-post/<int:pk>', views.AdminPostUpdate.as_view(), name='AdminPostUpdate'),
+    path('update/<int:pk>', views.BannerUpdate.as_view(), name='BannerUpdate'),
 
+
+    path('lista-foto', views.FotoBannerListView.as_view(), name='FotoBannerListView'),
+    path('novo-foto', views.FotoCreateView.as_view(), name='FotoCreateView'),
+    path('delete-foto/<int:pk>', views.FotorDelete.as_view(), name='FotorDelete'),
+    path('update-foto/<int:pk>', views.FotoUpdate.as_view(), name='FotoUpdate'),
 
 ]
